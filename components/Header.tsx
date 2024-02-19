8d0dc5e899db86b495dd36e7f3268ab43e28d58f
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-02-19 15:57:18 +0000

    f/ wave animation on bottom of banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import React from 'react';
 import { FaFacebook } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
+import { CiFacebook } from 'react-icons/ci';
 
 function Header() {
   return (
-    <div className="flex justify-between w-full py-3 xl:py-5 bg-white px-3 sm:px-5 lg:px-10">
+    <div className="fixed top-0 flex justify-between w-full py-3 xl:py-5 bg-white px-3 sm:px-5 lg:px-10 z-10">
       {/* Logo */}
       <div className="flex items-center ">
         <p className="uppercase tracking-widest text-xs md:text-sm xl:text-lg text-gray-700">
@@ -14,11 +15,17 @@ function Header() {
       </div>
       {/* nav */}
       <nav className="flex items-center space-x-5">
-        <Link href="https://www.facebook.com/atelierdarelacao">
-          <FaFacebook className="h-6 w-6 xl:h-8 xl:w-8 text-gray-700" />
+        <Link
+          href="https://www.facebook.com/atelierdarelacao"
+          className="p-1 bg-greenColor rounded-full"
+        >
+          <CiFacebook className="h-6 w-6 xl:h-8 xl:w-8 text-white" />
         </Link>
-        <Link href="https://www.instagram.com/atelierdarelacao">
-          <FaInstagram className="h-6 w-6 xl:h-8 xl:w-8 text-gray-700" />
+        <Link
+          href="https://www.instagram.com/atelierdarelacao"
+          className="p-1 bg-greenColor rounded-full"
+        >
+          <FaInstagram className="h-6 w-6 xl:h-8 xl:w-8 text-white" />
         </Link>
       </nav>
     </div>
