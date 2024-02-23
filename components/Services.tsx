c6d4d75dfb35e4f568782f1d3217dbdde9af9386
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-02-23 16:15:48 +0000

    Icon services section changes

--- components/Services.tsx
+++ components/Services.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import image2 from '../public/Image2.jpg';
 import Image from 'next/image';
 
-import icon1 from '@/public/BrainIcon.png';
+import icon1 from '@/public/Icon1.png';
 import icon2 from '@/public/Icon2.png';
 import icon3 from '@/public/Icon3.png';
 import icon4 from '@/public/Icon4.png';
@@ -23,41 +23,18 @@ function Services() {
         <h2 className="sectionTitle text-white">Serviços</h2>
       </div>
       {/* Service tiles */}
-      <div className="flex flex-col md:grid grid-flow-row grid-cols-2 gap-8 lg:gap-16 xl:gap-20 items-center justify-center py-8 lg:py-16 xl:py-20 pb-10 space-y-16 md:space-y-0 rounded-md ">
+      <div className="flex flex-col md:grid grid-flow-row grid-cols-2 gap-8 lg:gap-16 xl:gap-20 items-center justify-center py-10 lg:py-16 xl:py-20 pb-10 space-y-5 md:space-y-0 rounded-md ">
         <div className="serviceTiles">
           <Image
             className="serviceImage "
+            width={68}
+            height={68}
             src={icon3}
             alt="Outline image on children"
           />
           <p>Consulta de Psicologia e Psicoterapia de Crianças</p>
           <p className="servicePrice">Sessão: 50€</p>
-          <button className="button2">Agendar</button>
-        </div>
-        <div className="serviceTiles">
-          <Image
-            className="serviceImage"
-            width={68}
-            height={68}
-            src={icon2}
-            alt="Outline image of a heart and a brain"
-          />
-          <p>Consulta de Psicologia e Psicoterapia de Adolescentes</p>
-          <p className="servicePrice">Sessão: 50€</p>
-          <button className="button2">Agendar</button>
-        </div>
-
-        <div className="serviceTiles">
-          <Image
-            className="serviceImage"
-            width={68}
-            height={68}
-            src={icon4}
-            alt="Outline image of holding hands over a heart"
-          />
-          <p>Consulta de Psicologia e Psicoterapia de Adultos</p>
-          <p className="servicePrice">Sessão: 50€</p>
-          <button className="button2">Agendar</button>
+          {/* <button className="button2">Agendar</button> */}
         </div>
         <div className="serviceTiles">
           <Image
@@ -65,11 +42,36 @@ function Services() {
             width={68}
             height={68}
             src={icon5}
+            alt="Outline image of a heart and a brain"
+          />
+          <p>Consulta de Psicologia e Psicoterapia de Adolescentes</p>
+          <p className="servicePrice">Sessão: 50€</p>
+          {/* <button className="button2">Agendar</button> */}
+        </div>
+
+        <div className="serviceTiles">
+          <Image
+            className="h-24 md:h-28 w-auto"
+            width={68}
+            height={68}
+            src={icon4}
+            alt="Outline image of holding hands over a heart"
+          />
+          <p>Consulta de Psicologia e Psicoterapia de Adultos</p>
+          <p className="servicePrice">Sessão: 50€</p>
+          {/* <button className="button2">Agendar</button> */}
+        </div>
+        <div className="serviceTiles">
+          <Image
+            className="serviceImage"
+            width={68}
+            height={68}
+            src={icon1}
             alt="Outline image of a brain inside a human head"
           />
           <p>Avaliação Psicológica</p>
           <p className="servicePrice">Sessão: 50€</p>
-          <button className="button2">Agendar</button>
+          {/* <button className="button2">Agendar</button> */}
         </div>
       </div>
     </div>
