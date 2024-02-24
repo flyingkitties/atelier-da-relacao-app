a3ecb027a35154b61dbb44878c91b07796e9c789
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-02-24 09:41:45 +0000

    b/ fix on carousel photo slice section

--- components/PhotoSlide.tsx
+++ components/PhotoSlide.tsx
@@ -1,25 +1,88 @@
 'use client';
-import { motion, useTransform, useScroll } from 'framer-motion';
-import { useRef } from 'react';
+import {
+  motion,
+  useTransform,
+  useScroll,
+  Variants,
+  MotionProps,
+  useViewportScroll,
+} from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import { throttle } from 'throttle-debounce-ts';
+
+function useElementViewportPosition(ref: React.RefObject<HTMLElement>) {
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    if (!ref || !ref.current) return;
+
+    const pageHeight = document.body.scrollHeight;
+    const start = ref.current.offsetTop;
+    const end = start + ref.current.offsetHeight;
+
+    setPosition([start / pageHeight, end / pageHeight]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { position };
+}
+const slideAnimation: MotionProps = {
+  variants: {
+    full: { backgroundColor: '#663399' },
+    partial: { backgroundColor: '#808080' },
+  },
+  initial: 'partial',
+  whileInView: 'full',
+  viewport: { amount: 1, once: true },
+};
 
 const PhotoSlide = () => {
-  const targetRef = useRef<HTMLDivElement | null>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
+  const carouselRef = useRef<HTMLDivElement>(null);
+  const { position } = useElementViewportPosition(ref);
+  const [carouselEndPosition, setCarouselEndPosition] = useState(0);
+  const { scrollYProgress } = useViewportScroll();
 
-  const { scrollYProgress } = useScroll({
-    target: targetRef,
-  });
+  const x = useTransform(scrollYProgress, position, [0, carouselEndPosition]);
+  useEffect(() => {
+    window.addEventListener('scroll', () =>
+      console.log({ scrollYProgress: scrollYProgress, scrollY }),
+    );
+  }, []);
 
-  const x = useTransform(scrollYProgress, [0, 1], ['100%', '1%']);
+  useEffect(() => {
+    if (!carouselRef || !carouselRef.current) return;
+    const parent = carouselRef.current.parentElement;
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
 
-  console.log('x:', x);
+    const resetCarouselEndPosition = () => {
+      if (carouselRef && carouselRef.current) {
+        const newPosition =
+          carouselRef.current.clientWidth -
+          window.innerWidth +
+          scrollbarWidth +
+          (parent as HTMLElement).offsetLeft * 2;
+
+        setCarouselEndPosition(-newPosition);
+      }
+    };
+
+    resetCarouselEndPosition();
+    const handleResize = throttle(10, resetCarouselEndPosition);
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <section
-      ref={targetRef}
-      className="relative p-16 bg-darkGreenColor"
+      ref={ref}
+      className="relative h-[300vh] bg-darkGreenColor"
     >
-      <div className="sticky top-0 flex items-center overflow-x-hidden">
+      <div className="sticky top-0 h-screen w-[100%] flex items-center overflow-x-hidden">
         <motion.div
+          ref={carouselRef}
           style={{ x }}
           className="flex gap-4"
         >
@@ -41,7 +104,7 @@ const Card = ({ card }: { card: CardProps }) => {
   return (
     <div
       key={card.id}
-      className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
+      className="group relative h-[320px] w-[320px] overflow-hidden bg-neutral-200"
     >
       <div
         style={{
