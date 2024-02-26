c039395241c72c8135bf6c314742ccaf80941b3d
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-02-26 17:57:59 +0000

    changes to footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,35 +14,16 @@ function Footer() {
   return (
     <div>
       <div className="relative top-16 pt-6 bg-darkGreenColor text-white text-xs ">
-        {/* Socials */}
-        <nav className="flex items-center justify-center space-x-5">
-          <Link
-            href="https://www.facebook.com/atelierdarelacao"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-[6px] bg-gray-100 rounded-full"
-          >
-            <RiFacebookCircleLine className="h-6 w-6 xl:h-8 xl:w-8 text-darkGreenColor" />
-          </Link>
-          <Link
-            href="https://www.instagram.com/atelierdarelacao"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="p-2 bg-gray-100 rounded-full"
-          >
-            <FaInstagram className="h-5 w-5 xl:h-7 xl:w-7 text-darkGreenColor" />
-          </Link>
-        </nav>
         {/* Contact info */}
-        <div className="flex flex-col items-center justify-center pt-5 md:px-[10%] 2xl:px-[20%]">
-          <div className="px-20">
+        <div className="flex flex-col lg:grid grid-cols-2 items-center justify-center md:px-[10%] 2xl:px-[20%]">
+          <div className="px-20 lg:px-0">
             <Image
-              className="w-full h-auto"
+              className="w-full h-auto lg:h-full lg:w-auto"
               src={logo}
               alt="Atelier da relação Logo"
             />
           </div>
-          <div className="flex flex-col items-start justify-start w-full space-y-3 px-5 pb-3">
+          <div className="flex flex-col items-start justify-start  space-y-3 px-5 pb-3">
             {/* Hora de atendimento */}
             <div className="contactInfoDiv">
               <IoTimeOutline className="h-5 w-5 xl:h-7 xl:w-7 text-gray-100" />
@@ -70,10 +51,32 @@ function Footer() {
             </div>
           </div>
         </div>
+        <div className="flex items-center justify-center py-5">
+          {/* Socials */}
+          <nav className="flex items-center justify-center space-x-5">
+            <Link
+              href="https://www.facebook.com/atelierdarelacao"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-[3px] bg-gray-100 rounded-full"
+            >
+              <RiFacebookCircleLine className="h-6 w-6 xl:h-8 xl:w-8 text-darkGreenColor" />
+            </Link>
+            <Link
+              href="https://www.instagram.com/atelierdarelacao"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-1 bg-gray-100 rounded-full"
+            >
+              <FaInstagram className="h-5 w-5 xl:h-7 xl:w-7 text-darkGreenColor" />
+            </Link>
+          </nav>
+        </div>
         {/* Terms and privacy */}
-        <div className="flex justify-center p-5 text-xs px-5 md:px-[10%] 2xl:px-[20%]">
-          <p className="px-5">Termos e Condições</p>
-          <p className="px-5">Política de Privacidade</p>
+        <div className="flex justify-center p-5 text-xs px-5 md:px-[10%] 2xl:px-[20%] space-x-2">
+          <p className="sm:px-5">Termos e Condições</p>
+          <p>|</p>
+          <p className="sm:px-5">Política de Privacidade</p>
         </div>
         {/* Copyright */}
         <div className="flex justify-center items-center px-5 text-[9px] border-t-[0.4px] border-gray-400 py-5">
