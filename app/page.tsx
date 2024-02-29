6e56381ad701c3a4f2ec60df39c8ef4d2812f90e
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-02-29 11:38:11 +0000

    chantes to font size team section

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
       <section id="team">
         <Team />
       </section>
-      <section id="quote">
-        <Quote />
-      </section>
       <section id="appointments">
         <Appointments />
+      </section>
+      <section id="quote">
+        <Quote />
       </section>
       <section id="location">
         <WhereWeAre />
