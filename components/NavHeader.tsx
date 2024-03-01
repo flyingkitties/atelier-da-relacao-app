0d7ac3854740ed7cc68b9fa3a4890564d13fccec
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-03-01 11:12:53 +0000

    bug on nav bar button focus

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -3,48 +3,48 @@ import Link from 'next/link';
 import React from 'react';
 
 function NavHeader() {
-  const scrollSmooth = (
-    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-    id: string,
-  ) => {
-    e.preventDefault();
-    document?.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
-  };
+  // const scrollSmooth = (
+  //   e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  //   id: string,
+  // ) => {
+  //   // e.preventDefault();
+  //   document?.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
+  // };
 
   return (
     <div className="flex flex-wrap justify-center gap-3 px-5 py-5">
-      <Link
-        href="#aboutUs"
-        onClick={(e) => {
-          scrollSmooth(e, 'aboutUs');
-        }}
-      >
+      <Link href="#aboutUs">
         <button className="button1">Sobre nós</button>
       </Link>
       <Link
         href="#services"
-        onClick={(e) => {
-          scrollSmooth(e, 'services');
-        }}
+        // onClick={(e) => {
+        //   scrollSmooth(e, 'services');
+        // }}
       >
         <button className="button1">Serviços</button>
       </Link>
       <Link
         href="#team"
-        onClick={(e) => {
-          scrollSmooth(e, 'team');
-        }}
+        // onClick={(e) => {
+        //   scrollSmooth(e, 'team');
+        // }}
       >
         <button className="button1">Equipa</button>
       </Link>
-      <Link href="#location">
+      <Link
+        href="#location"
+        // onClick={(e) => {
+        //   scrollSmooth(e, 'appointments');
+        // }}
+      >
         <button className="button1">Onde estamos</button>
       </Link>
       <Link
         href="#appointments"
-        onClick={(e) => {
-          scrollSmooth(e, 'appointments');
-        }}
+        // onClick={(e) => {
+        //   scrollSmooth(e, 'appointments');
+        // }}
       >
         <button className="button1 bg-darkGreenColor border-darkGreenColor hover:text-black hover:bg-transparent text-white ">
           <p>Marcações</p>
