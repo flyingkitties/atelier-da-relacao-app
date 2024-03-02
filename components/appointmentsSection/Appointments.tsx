868feb44beef3bf3ae658c263db36c57c98a2827
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-03-02 17:32:45 +0000

    changes to form font and padding

--- components/appointmentsSection/Appointments.tsx
+++ components/appointmentsSection/Appointments.tsx
@@ -14,11 +14,11 @@ function Appointments() {
        bg-gray-50 shadow-lg my-[3%] md:my-[5%] max-w-xl"
         >
           {/* Form */}
-          <div className="m-3 md:m-5 md:mt-10 p-3 pb-0 text-center">
+          <div className="m-3 md:m-5 mb-0 md:mt-10 p-3 pb-0 text-center">
             <h2 className="max-[320px]:text-base text-lg lg:text-xl xl:text-2xl font-bold mb-5">
               Entre em contacto conosco
             </h2>
-            <h3 className="teamJob font-light">
+            <h3 className="text-xs md:text-sm xl:text-base font-light">
               Preencha o formulário e entraremos em contacto consigo para
               agendamento da sua consulta ou para esclarecimento de
               questões/pedido de informação
