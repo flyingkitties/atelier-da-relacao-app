a756727764d9d756366eb6f189c85a2137403b44
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2024-04-11 12:07:24 +0100

    fix on fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,16 +39,16 @@ export default function RootLayout({
     >
       <head>
         <Script
-          id="GoogleTagManager"
+          id="Gtm"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5PR8TRDX');`,
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5PR8TRDX');`,
           }}
-        />
+        ></Script>
       </head>
       <body className={inter.className}>
         {children}
